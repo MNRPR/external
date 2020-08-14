bc2840dfdf97d3a4a28d4efd5dcff7e38eb3740a
manjunatharaop@gmail.com <manjunatharaop@gmail.com>
2020-08-14 16:07:55 -0700

    Ver 1.1

--- serach_file.py
+++ serach_file.py
@@ -8,11 +8,11 @@ while True:
     file_list = os.listdir(cur_dir)
     parent_dir = os.path.dirname(cur_dir)
     if file_name in file_list:
-        print "File found in: ", cur_dir
+        print "Wow ! the file found in: ", cur_dir
         break
     else:
         if cur_dir == parent_dir: #if dir is root dir
-            print "File not found"
+            print "Sorry the file not found"
             break
         else:
             cur_dir = parent_dir
