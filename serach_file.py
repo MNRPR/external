415ebc6b18482984858efe7fb4ea4c10bdc9ddd1
manjunatharaop@gmail.com <manjunatharaop@gmail.com>
2020-08-14 16:07:03 -0700

    Ver 1.0

--- serach_file.py
+++ serach_file.py
@@ -1,14 +1,14 @@
 import os
 import os.path
 
-file_name = "test.txt" #file to be searched
-cur_dir = os.getcwd() # Dir from where search starts can be replaced with any path
+file_name = "manju.doc" #The file which need to be searched
+cur_dir = os.getcwd() # The search will start from this directory
 
 while True:
     file_list = os.listdir(cur_dir)
     parent_dir = os.path.dirname(cur_dir)
     if file_name in file_list:
-        print "File Exists in: ", cur_dir
+        print "File found in: ", cur_dir
         break
     else:
         if cur_dir == parent_dir: #if dir is root dir
