98cf673351ef3e554758d636109ec82529bacd94
manjunatharaop@gmail.com <manjunatharaop@gmail.com>
2020-08-16 22:31:46 -0700

    Ver 0.1

---
+++ Log_Search.py
@@ -0,0 +1,13 @@
+import re
+
+log_file_path = r"C:\ios logs\sfbios.log"
+regex = '(<property name="(.*?)">(.*?)<\/property>)'
+
+match_list = []
+with open(log_file_path, "r") as file:
+    for line in file:
+        for match in re.finditer(regex, line, re.S):
+            match_text = match.group()
+            match_list.append(match_text)
+            print
+            match_text
