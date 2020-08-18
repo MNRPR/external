439976532700edbde373f0d73175f55ae792bfb0
manjunatharaop@gmail.com <manjunatharaop@gmail.com>
2020-08-17 18:47:57 -0700

    Ver 0.6

--- Log_Search.py
+++ Log_Search.py
@@ -2,11 +2,11 @@ import re
 
 log_file_path = r"/var/log/system.log"
 #regex = '(<property name="(.*?)">(.*?)<\/property>)'
-regex = "[a-zA-Z0-9]+@[a-zAZ]+\.]"
+regex = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|net)"
 match_list = []
 with open(log_file_path, "r") as file:
     for line in file:
-        for match in re.finditer(regex, line, re.S):
+        for match in re.search(regex, str):
             match_text = match.group()
             match_list.append(match_text)
             print
